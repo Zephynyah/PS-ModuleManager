c7fc4cdb2715808a087711424cf856c6370674d4
Zephynyah <jah_hick@yahoo.com>
2026-02-16 15:42:30 -0500

    Create PS-ModuleManager.ps1

---
+++ PS-ModuleManager.ps1
@@ -0,0 +1,7 @@
+# Import the module
+Import-Module .\PS-ModuleManager.psd1 -Verbose -Force
+
+# Launch the GUI
+# Show-ModuleManagerGUI
+
+Show-ModuleManagerGUI -WindowStartupLocation CenterOwner -WindowState Maximized
